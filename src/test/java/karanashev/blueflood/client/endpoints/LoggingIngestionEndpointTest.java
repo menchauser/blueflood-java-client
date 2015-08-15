67fe159cf5334dd2b0ca184f9b521563b7cbff96
Mukhamed Karanashev <menchauser@gmail.com>
2015-08-15 23:08:37 +0400

    Reorganized tests

--- src/test/java/karanashev/blueflood/client/endpoints/LoggingIngestionEndpointTest.java
+++ src/test/java/karanashev/blueflood/client/endpoints/LoggingIngestionEndpointTest.java
@@ -1,8 +1,6 @@
-package karanashev.blueflood.client;
+package karanashev.blueflood.client.endpoints;
 
 import karanashev.blueflood.client.datetime.DefaultTimeInterval;
-import karanashev.blueflood.client.endpoints.IngestionEndpoint;
-import karanashev.blueflood.client.endpoints.ProfilingIngestionEndpoint;
 import karanashev.blueflood.client.model.DataPoints;
 import org.joda.time.DateTime;
 import org.junit.Test;
@@ -15,16 +13,17 @@ import static org.mockito.Mockito.*;
  * Author: Karanashev
  * Date: 30.07.15
  */
-public class ProfilingIngestionEndpointTest {
+public class LoggingIngestionEndpointTest {
 
     @Test
-    public void profilingIngestionInvokesInnerIngestion() throws Exception {
+    public void loggingIngestionInvokesInnerIngestion() throws Exception {
         IngestionEndpoint innerIngestionEndpoint = mock(IngestionEndpoint.class);
-        ProfilingIngestionEndpoint profilingIngester = new ProfilingIngestionEndpoint(innerIngestionEndpoint);
+        LoggingIngestionEndpoint loggingIngester = new LoggingIngestionEndpoint(innerIngestionEndpoint);
         DataPoints dataPoints = new DataPoints().add(new DateTime(), DefaultTimeInterval.HOUR.value(), BigDecimal.ONE, "example.one");
 
-        profilingIngester.ingest(dataPoints);
+        loggingIngester.ingest(dataPoints);
 
         verify(innerIngestionEndpoint, only()).ingest(dataPoints);
     }
+
 }
--- src/test/java/karanashev/blueflood/client/endpoints/ProfilingIngestionEndpointTest.java
+++ src/test/java/karanashev/blueflood/client/endpoints/ProfilingIngestionEndpointTest.java
@@ -1,8 +1,6 @@
-package karanashev.blueflood.client;
+package karanashev.blueflood.client.endpoints;
 
 import karanashev.blueflood.client.datetime.DefaultTimeInterval;
-import karanashev.blueflood.client.endpoints.IngestionEndpoint;
-import karanashev.blueflood.client.endpoints.LoggingIngestionEndpoint;
 import karanashev.blueflood.client.model.DataPoints;
 import org.joda.time.DateTime;
 import org.junit.Test;
@@ -15,17 +13,16 @@ import static org.mockito.Mockito.*;
  * Author: Karanashev
  * Date: 30.07.15
  */
-public class LoggingIngestionEndpointTest {
+public class ProfilingIngestionEndpointTest {
 
     @Test
-    public void loggingIngestionInvokesInnerIngestion() throws Exception {
+    public void profilingIngestionInvokesInnerIngestion() throws Exception {
         IngestionEndpoint innerIngestionEndpoint = mock(IngestionEndpoint.class);
-        LoggingIngestionEndpoint loggingIngester = new LoggingIngestionEndpoint(innerIngestionEndpoint);
+        ProfilingIngestionEndpoint profilingIngester = new ProfilingIngestionEndpoint(innerIngestionEndpoint);
         DataPoints dataPoints = new DataPoints().add(new DateTime(), DefaultTimeInterval.HOUR.value(), BigDecimal.ONE, "example.one");
 
-        loggingIngester.ingest(dataPoints);
+        profilingIngester.ingest(dataPoints);
 
         verify(innerIngestionEndpoint, only()).ingest(dataPoints);
     }
-
 }
